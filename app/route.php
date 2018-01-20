d31b969b307b572c1386b9f65f817be4ce421c9a
franco <463024796@qq.com>
2018-01-20 17:32:33 +0800

    搭建静态页面，详细看route

--- app/route.php
+++ app/route.php
@@ -8,10 +8,46 @@
 // +----------------------------------------------------------------------
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+use think\Route;
 //调用extend的路由类
 $Route = new \Route;
 $Route->route_rule();
+//首页
+Route::get('/', function () {
+	return view('app/shop/view/index.html');
+});
+//商品详情页
+Route::get("/product", function() {
+    return view('app/shop/view/product.html');
+});
+//购物车
+Route::get("/checkout", function() {
+    return view('app/shop/view/checkout.html');
+});
+//登录
+Route::get("/login", function() {
+    return view('app/shop/view/login.html');
+});
+//contact
+Route::get("/contact", function() {
+    return view('app/shop/view/contact.html');
+});
+//collections
+Route::get("/collections", function() {
+    return view('app/shop/view/collections.html');
+});
+//payment
+Route::get("/payment", function() {
+    return view('app/shop/view/payment.html');
+});
+//register
+Route::get("/register", function() {
+    return view('app/shop/view/register.html');
+});
+//terms
+Route::get("/terms", function() {
+    return view('app/shop/view/terms.html');
+});
 return [
 	//也可以这里添加路由规则
 ];
